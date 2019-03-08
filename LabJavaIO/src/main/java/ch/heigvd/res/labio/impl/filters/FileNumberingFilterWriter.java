81b1e1aacf732fba9a9f634f7277bb67d6987133
zereddiamond <antoine.hun@bluewin.ch>
2019-03-08 12:25:04 +0100

    Tests passed for one line.

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -23,19 +23,44 @@ public class FileNumberingFilterWriter extends FilterWriter {
     super(out);
   }
 
+
+  //inspired by this website : https://stackoverflow.com/questions/17016242/reading-writing-files-in-java-and-adding-line-numbers
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    Integer lineNumber = new Integer(1);
+    /*StringBuilder sb = new StringBuilder();
+
+    sb.append(lineNumber.toString() + "\t");
+    sb.append(str);*/
+    //String newString = lineNumber.toString() + "\t" + str;
+    StringBuffer sb = new StringBuffer();
+
+    sb.append(lineNumber.toString());
+    sb.append("\t");
+    sb.append(str);
+    ++lineNumber;
+    sb.append(lineNumber.toString());
+    sb.append("\t");
+    len += 4;
+
+
+    String newString = sb.toString();
+
+    super.write(newString, off, len);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    super.write(cbuf, off, len);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    super.write(c);
   }
 
 }
